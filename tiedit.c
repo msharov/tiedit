84066498d0a2d3a36465004210d582a20a3306a8
Mike Sharov <msharov@users.sourceforge.net>
2014-01-07 19:56:27 -0500

    Navigation

--- tiedit.c
+++ tiedit.c
@@ -18,9 +18,13 @@
 
 #define KEY_ESCAPE	27
 
+static void FillRect (unsigned x, unsigned y, unsigned w, unsigned h);
+static void DrawLine (unsigned l);
 static void Draw (void);
 static void OnKey (unsigned key);
 
+static void EventLoop (void);
+static void InitUI (void);
 static void CleanupUI (void);
 static void OnQuitSignal (int sig);
 static void OnMsgSignal (int sig);
@@ -47,13 +51,21 @@ struct STerminfo {
     char*		strings;
 };
 
-//----------------------------------------------------------------------
+//}}}-------------------------------------------------------------------
+//{{{ Globals
 
 static struct STerminfo _info = {{0,0,0,0,0,0},NULL,NULL,NULL,NULL,NULL};
 static bool _quitting = false;
+static unsigned _topline = 0;
+static unsigned _selection = 0;
 
 //}}}-------------------------------------------------------------------
-//{{{ Terminfo data loading and editing
+//{{{ Utility functions
+
+static inline unsigned min (unsigned a, unsigned b)
+{
+    return (a < b ? a : b);
+}
 
 static void ReadBytes (int fd, void* buf, size_t bufsz)
 {
@@ -72,6 +84,9 @@ static void* Realloc (void* op, size_t nsz)
     }
     return (p);
 }
+
+//}}}-------------------------------------------------------------------
+//{{{ Terminfo data loading and editing
 
 static void LoadTerminfo (const char* tifile)
 {
@@ -101,32 +116,100 @@ static void LoadTerminfo (const char* tifile)
 //}}}-------------------------------------------------------------------
 //{{{ UI
 
+static void FillRect (unsigned x, unsigned y, unsigned w, unsigned h)
+{
+    for (unsigned j = 0; j < h; ++j) {
+	move (y+j, x);
+	for (unsigned i = 0; i < w; ++i)
+	    addch (' ');
+    }
+}
+
 static void DrawLine (unsigned l)
 {
-    mvprintw (l, 1, "Line %u\n", l);
+    mvprintw (l, 1, "Line %u", _topline+l);
 }
 
 static void Draw (void)
 {
     erase();
-    for (unsigned l = 0; l < (unsigned) LINES-1; ++l)
+    const unsigned nLines = _info.h.nBooleans + _info.h.nNumbers + _info.h.nStrings;
+    const unsigned nVisible = min (nLines, LINES-1), visselection = _selection-_topline;
+    for (unsigned l = 0; l < nVisible; ++l) {
+	if (visselection == l)
+	    attron (A_REVERSE);
+	FillRect (0, l, COLS, 1);
 	DrawLine (l);
+	if (visselection == l)
+	    attroff (A_REVERSE);
+    }
     attron (A_REVERSE);
-    move (LINES-1, 0);
-    for (unsigned c = 0; c < (unsigned) COLS; ++c)
-	addch (' ');
+    FillRect (0, LINES-1, COLS, 1);
     mvaddstr (LINES-1, 1, _info.name);
     attroff (A_REVERSE);
 }
 
 static void OnKey (unsigned key)
 {
-    if (key == 'q' || key == KEY_ESCAPE)
+    const unsigned nLines = _info.h.nBooleans + _info.h.nNumbers + _info.h.nStrings;
+    const unsigned pageSize = LINES-1;
+    if (key == KEY_ESCAPE || key == 'q')
 	_quitting = true;
+    else if (key == KEY_HOME || key == '0')
+	_selection = 0;
+    else if (key == KEY_END || key == 'G')
+	_selection = nLines-1;
+    else if (key == 'H')
+	_selection = _topline;
+    else if (key == 'M')
+	_selection = _topline+(pageSize-1)/2;
+    else if (key == 'L')
+	_selection = _topline+(pageSize-1);
+    else if ((key == KEY_UP || key == 'k') && _selection > 0)
+	--_selection;
+    else if ((key == KEY_DOWN || key == 'j') && _selection < nLines-1)
+	++_selection;
+    else if (key == KEY_PPAGE || key == 'b') {
+	if (_selection > pageSize)
+	    _selection -= pageSize;
+	else
+	    _selection = 0;
+    } else if (key == KEY_NPAGE || key == ' ') {
+	if (_selection + pageSize < nLines-1)
+	    _selection += pageSize;
+	else
+	    _selection = nLines-1;
+    }
+    if (_topline > _selection)
+	_topline = _selection;
+    if (_topline + pageSize-1 < _selection)
+	_topline = _selection - (pageSize-1);
 }
 
 //}}}-------------------------------------------------------------------
 //{{{ Process housekeeping
+
+static void InitUI (void)
+{
+    if (!initscr()) {
+	puts ("Error: unable to initialize UI");
+	exit (EXIT_FAILURE);
+    }
+    cbreak();
+    noecho();
+    keypad (stdscr, true);
+    curs_set (false);
+}
+
+static void EventLoop (void)
+{
+    while (!_quitting) {
+	Draw();
+	int key = getch();
+	if (key > 0)
+	    OnKey (key);
+    }
+}
 
 static void CleanupUI (void)
 {
@@ -185,16 +268,8 @@ int main (void)
 {
     InstallCleanupHandlers();
     LoadTerminfo ("/usr/share/terminfo/x/xterm");
-    if (!initscr()) {
-	puts ("Error: unable to initialize UI");
-	return (EXIT_FAILURE);
-    }
-    while (!_quitting) {
-	Draw();
-	int key = getch();
-	if (key > 0)
-	    OnKey (key);
-    }
+    InitUI();
+    EventLoop();
     return (EXIT_SUCCESS);
 }
 
